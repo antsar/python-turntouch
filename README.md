8209d74c5045840f498048dc9fc7436c7d5a6ad7
Anton Sarukhanov <code@ant.sr>
2018-06-05 22:52:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TurnTouch Python library
+
+This library provides support for the [Turn Touch](https://shop.turntouch.com/)
+bluetooth smart home remote.
+
+It is written in Python 3, originally for use with [Home Assistant](https://www.home-assistant.io/).
+
+# Status
+
+This is currently pre-alpha status. It is not usable.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    '''Read a file and return the contents.'''
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='TurnTouch',
+    version='0.1',
+    url='https://github.com/antsar/python-turntouch',
+    author='Anton Sarukhanov',
+    author_email='code@ant.sr',
+    description='Python library for the Turn Touch smart home remote',
+    license='MIT',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'bluepy==1.1.4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Home Automation',
+    ]
+)
